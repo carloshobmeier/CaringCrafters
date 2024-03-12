ce8230b3368b1fcb1f7e13c174546c85af0c8d5b
bptiago <125921557+bptiago@users.noreply.github.com>
2024-03-12 14:47:03 -0300

    Done index.php

---
+++ src/components/footer.php
@@ -0,0 +1,35 @@
+<div class="container">
+  <footer class="row row-cols-5 py-5 my-5 border-top">
+    <div class="col">
+      <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+        CaringCrafters
+      </a>
+      <p class="text-muted">© 2024</p>
+    </div>
+    <!-- Espaco em branco, não apagar -->
+    <div class="col"></div>
+    <div class="col">
+      <h5>Ajuda</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Perguntas frequentes</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Sobre nós</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Contato</a></li>
+      </ul>
+    </div>
+    <div class="col">
+      <h5>Serviços</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Organizações</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Voluntariado</a></li>
+      </ul>
+    </div>
+
+    <div class="col">
+      <h5>Políticas</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Termo de privacidade</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Políticas de uso</a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
