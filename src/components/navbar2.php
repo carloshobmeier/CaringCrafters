fc8b3014a197dcc9441f86f24b9bd90e51baa6ae
carloshobmeier <carloshobmeier@hotmail.com>
2024-03-18 20:08:12 -0300

    Fix navbar inside container

--- src/components/navbar2.php
+++ src/components/navbar2.php
@@ -11,25 +11,26 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-sm navbar-light bg-light">
-  <div class="container-fluid">
-    <img src="./brand_identity/Logo_PNG-verde.png" class="navbar-brand" style="max-width:80px">Caring Crafters
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <div class="rightside ms-auto">
-        <div id="navButtons" class="buttonsNav">
-          <button class="btn btn-outline-success">Institutions</button>
-          <button class="btn btn-outline-success">Opportunities</button>
-          <button class="btn btn-outline-success">Browse</button>
+<div class="container">
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <div class="container-fluid">
+      <img src="./brand_identity/Logo_PNG-verde.png" class="navbar-brand" style="max-width:80px">Caring Crafters
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="rightside ms-auto">
+          <div id="navButtons" class="buttonsNav">
+            <button class="btn btn-outline-success">Institutions</button>
+            <button class="btn btn-outline-success">Opportunities</button>
+            <button class="btn btn-outline-success">Browse</button>
+          </div>
+          <img src="./resources/profile-picture.jpeg" alt="User Avatar" class="navbar-profile-pic">
         </div>
-        <img src="./resources/profile-picture.jpeg" alt="User Avatar" class="navbar-profile-pic">
       </div>
     </div>
-  </div>
-</nav>
-
+  </nav>
+</div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
