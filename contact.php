894989db1d6115c645e6c4826c5e855cfd3e0be0
carloshobmeier <carloshobmeier@hotmail.com>
2024-03-18 19:30:45 -0300

    navbar2 with bootstrap and responsive contact.php

--- contact.php
+++ contact.php
@@ -6,6 +6,8 @@
   <title>Contact</title>
   <link rel="stylesheet" href="./style/contact.css">
   <link rel="stylesheet" href="./style/setup.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+
 </head>
 <body>
   <?php include('./src/components/navbar2.php') ?>
@@ -16,20 +18,28 @@
     <p>Get in touch with us. We're here to assist you in finding the perfect volunteer opportunity.</p>
   </div>
 
-  <div class="content">
-    <form>
-      <h5>Your name</h5>
-      <input type="text" placeholder="Full name">
-      <h5>Your email</h5>
-      <input type="email" placeholder="email@example.com">
-      <h5>Subject</h5>
-      <input type="text" placeholder="Subject">
-      <h5>Message</h5>
-      <input type="text" placeholder="Type your message here"></input>
-      <button type="submit">Send</button>
-    </form>
+  <div class="container" style="margin-top:40px; margin-bottom:40px">
+    <div class="row">
 
-<iframe src="https://maps.google.com/?q=-25.451663438476608,-49.25029431341601&output=svembed"></iframe>
+      <div class="col-md-6">
+        <form>
+          <h5>Your name</h5>
+          <input type="text" class="form-control mb-3" placeholder="Full name">
+          <h5>Your email</h5>
+          <input type="email" class="form-control mb-3" placeholder="email@example.com">
+          <h5>Subject</h5>
+          <input type="text" class="form-control mb-3" placeholder="Subject">
+          <h5>Message</h5>
+          <textarea class="form-control mb-3" rows="5" placeholder="Type your message here"></textarea>
+          <button type="submit" class="btn btn-success">Send</button>
+        </form>
+      </div>
+   
+      <div class="col-md-6" style="margin-top:10px; margin-bottom:10px">
+        <iframe src="https://maps.google.com/?q=-25.451663438476608,-49.25029431341601&output=svembed" style="width:100%; height:100%"></iframe>
+      </div>
+    </div>
+  </div>
 
   </div>
 
