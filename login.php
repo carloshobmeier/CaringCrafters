62e5841cbb92c552beda668a54818f9ff326eb46
Rafael Gabardo <rggabardo@gmail.com>
2024-03-19 14:29:44 -0300

    Adding a button to the log in form

--- login.php
+++ login.php
@@ -11,18 +11,21 @@
 <body>
     <main>
         <div class="container">
-            <div class="card">
+            <div class="card rounded-3">
                 <div class="card-body">
                     <h1>Log In:</h1>
                     <p>Lorem ipsum dolor sit.</p>
-                    <div class="mb-3">
-                        <label for="formGroupExampleInput" class="form-label">Username:</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput">
-                    </div>
-                    <div class="mb-3">
-                        <label for="formGroupExampleInput2" class="form-label">Password</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput2">
-                    </div>
+                    <form action="#" method="get">
+                        <div class="mb-3">
+                            <label for="formGroupExampleInput" class="form-label">Username:</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput">
+                        </div>
+                        <div class="mb-3">
+                            <label for="formGroupExampleInput2" class="form-label">Password:</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput2">
+                        </div>
+                        <button class="btn btn-success ">Log In</button>
+                    </form>
                 </div>
             </div>
         </div>
