5e35d95d9d032d86f46889b62753b1eed474526a
Rafael Gabardo <rggabardo@gmail.com>
2024-03-19 12:27:30 -0300

    Adicionando Log In Page

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./style/login.css">
+    <link rel="stylesheet" href="./style/setup.css">
+</head>
+<body>
+    <main>
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                    <h1>Log In:</h1>
+                    <p>Lorem ipsum dolor sit.</p>
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput" class="form-label">Username:</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput">
+                    </div>
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput2" class="form-label">Password</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput2">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
