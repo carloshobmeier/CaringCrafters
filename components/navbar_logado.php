eee70028c19a74f97dc51298beccc7060a11e33f
carloshobmeier <carloshobmeier@hotmail.com>
2024-04-15 20:33:57 -0300

    adicionado modal de voluntário

--- components/navbar_logado.php
+++ components/navbar_logado.php
@@ -24,14 +24,7 @@
             <button class="btn btn-outline-success">Voluntários</button>
             <button class="btn btn-outline-success">Navegue</button>
           </div>
-          <a href="<?php 
-          $type = $_SESSION['tipoCadastro'];
-          if($type == "usuario"){
-            echo "profile_volunteer.php";
-          }else{
-            echo "profile_institution.php";
-          }
-          ?>"><img src="./assets/images/profile-picture.jpeg" alt="User Avatar" class="navbar-profile-pic"></a>
+          <a href="#" id="profileLink"><img src="./assets/images/profile-picture.jpeg" alt="User Avatar" class="navbar-profile-pic"></a>
         </div>
       </div>
     </div>
@@ -39,7 +32,46 @@
 </div>
 
 
+<!-- Modal -->
+<div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="profileModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="profileModalLabel">Perfil</h5>
+      </div>
+      <div class="modal-body">
+        <!-- Conteúdo do modal: imagem do usuário e botões -->
+        <div class="modal-buttons text-center" style="display: flex; flex-direction: column; align-items: center;">
+          <img src="./assets/images/profile-picture.jpeg" alt="User Avatar" class="modal-profile-pic elemento" width="50%" style="border-radius: 50%;">
+          <div>
+            <a href="profile_volunteer.php" class="btn btn-success elemento">Ver Perfil</a>
+            <button type="button" class="btn btn-success elemento" id="logoutButton">Sair</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+<script>
+  // Adicionamos um evento de clique ao link do perfil
+  document.getElementById('profileLink').addEventListener('click', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    // Exibe o modal
+    var myModal = new bootstrap.Modal(document.getElementById('profileModal'));
+    myModal.show();
+  });
+</script>
+
+<style>
+  .elemento {
+    margin: 15px;
+  }
+
+
+</style>
 
 </body>
 </html>
