27d34a75dd8ff4e90af09726a51de91ebede3d27
Amanda Queiroz <amandaq.sobral@gmail.com>
2024-04-15 00:28:03 -0300

    página de termos e condições de uso da plataforma

--- components/footer.php
+++ components/footer.php
@@ -28,7 +28,7 @@
       <h5>Políticas</h5>
       <ul class="nav flex-column">
         <li class="nav-item mb-2"><a href="privacy.php" class="color-white nav-link p-0">Política de privacidade</a></li>
-        <li class="nav-item mb-2"><a href="#" class="color-white nav-link p-0">Termos de serviço</a></li>
+        <li class="nav-item mb-2"><a href="terms.php" class="color-white nav-link p-0">Termos de serviço</a></li>
       </ul>
     </div>
   </footer>
