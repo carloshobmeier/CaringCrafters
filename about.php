52de4b13840b7d9725bf176546ec1aef6f1d8993
carloshobmeier <carloshobmeier@hotmail.com>
2024-04-14 22:39:25 -0300

    correções de redação e de indentação no código

--- about.php
+++ about.php
@@ -38,7 +38,7 @@ if (!isset($_SESSION['id'])) {
 
         <p>Bem-vindo ao CaringCrafters, onde a <span>compaixão</span> se torna <span>ação</span>.</p>
 
-        <p>Nossa história começa em 2024, impulsionada por um ideal simples: cada gesto de bondade tem o poder de transformar o mundo. A partir desse princípio, embarcamos em uma jornada para unir corações apaixonados a oportunidades de causar <span>impacto social</span>.</p>
+        <p>Nossa história começa em 2024, impulsionada por um ideal simples: cada gesto de bondade tem o poder de transformar o mundo. A partir desse princípio, embarcamos em uma jornada para unir corações apaixonados e iniciativas que causam <span>impacto social</span>.</p>
 
         <p>Nossa equipe é diversa e comprometida, unida por um propósito comum: promover o bem. Com experiência em envolvimento comunitário e iniciativas sociais, trabalhamos juntos para inspirar <span>mudanças</span> significativas.</p>
 
@@ -47,33 +47,36 @@ if (!isset($_SESSION['id'])) {
         <p>No CaringCrafters, não somos apenas uma plataforma - somos um <span>movimento</span>. Juntos, estamos tecendo uma rede de cuidado e compaixão que se estende por todo lugar.</p>
 
         <p>Junte-se a nós nesta jornada rumo a um mundo melhor. Juntos, vamos transformar a empatia em uma forma de viver.</p>
-</div>
+      </div>
 
-        <div class="col-md-5 direita" style="margin-top:10px; margin-bottom:10px">
-          <img src="./assets/images/cleaning.jpeg" alt="" width="90%">
-        </div>
-<div class="row">
-<div class="col-md-6 esquerda">
-
-  <h1 class="titulo">NOSSA MISSÃO</h1>
-  
-  <p>Na CaringCrafters, nossa missão transcende simples palavras - é um compromisso enraizado na <span>essência</span> do que somos. Buscamos incansavelmente um mundo onde a compaixão e a solidariedade são os pilares de todas as interações humanas. Nossa missão é mais do que uma declaração; é um <span>chamado à ação</span> para todos os corações generosos que desejam deixar um legado duradouro de bondade.</p>
-  
-  <p>Acreditamos que a verdadeira mudança começa com cada indivíduo. É por isso que nos dedicamos a <span>conectar</span> pessoas apaixonadas, o que acreditamos caracterizar o recurso necessário para causar um impacto significativo em suas comunidades e além. Da cidade mais movimentada ao vilarejo mais remoto, buscamos alcançar e tocar cada vida com um gesto de bondade.</p>
-  
-  <p>Nossa missão vai além de simplesmente oferecer ajuda; aspiramos a cultivar uma cultura de empatia e colaboração. Acreditamos no poder transformador das conexões humanas e na importância de unir forças para enfrentar os desafios globais que enfrentamos. Somos uma comunidade de agentes de mudança, cada um contribuindo para um mosaico de compaixão que torna o mundo um lugar mais luminoso.</p>
-  
-  <p>Junte-se a nós nesta jornada de corações unidos, onde cada ação, por menor que seja, tem o potencial de fazer uma diferença monumental. Nossa missão é clara: <span>inspirar</span>, <span>capacitar</span> e <span>mobilizar</span>, tornando a compaixão uma força inegável para o bem em nosso mundo.</p>
-
-</div>
-  <div class="col-md-5 direita" style="margin-top:10px; margin-bottom:10px">
-  <img src="./assets/images/recycling.jpeg" alt="" width="90%">
-        </div>
+      <div class="col-md-5 direita" style="margin-top:10px; margin-bottom:10px">
+        <img src="./assets/images/cleaning.jpeg" alt="" width="90%">
+      </div>
+    </div>
 
 
 
-<div class="row">
-<div class="col-md-6 esquerda">
+    <div class="row">
+      <div class="col-md-6 esquerda">
+
+        <h1 class="titulo">NOSSA MISSÃO</h1>
+        
+        <p>Na CaringCrafters, nossa missão transcende simples palavras - é um compromisso enraizado na <span>essência</span> do que somos. Buscamos incansavelmente um mundo onde a compaixão e a solidariedade são os pilares de todas as interações humanas. Nossa missão é mais do que uma declaração; é um <span>chamado à ação</span> para todos os corações generosos que desejam deixar um legado duradouro de bondade.</p>
+        
+        <p>Acreditamos que a verdadeira mudança começa com cada indivíduo. É por isso que nos dedicamos a <span>conectar</span> pessoas apaixonadas, o que acreditamos caracterizar o recurso necessário para causar um impacto significativo em suas comunidades e além. Da cidade mais movimentada ao vilarejo mais remoto, buscamos alcançar e tocar cada vida com um gesto de bondade.</p>
+        
+        <p>Nossa missão vai além de simplesmente oferecer ajuda; aspiramos cultivar uma cultura de empatia e colaboração. Acreditamos no poder transformador das conexões humanas e na importância de unir forças para enfrentar os desafios globais que enfrentamos. Somos uma comunidade de agentes de mudança, cada um contribuindo para um mosaico de compaixão que torna o mundo um lugar mais luminoso.</p>
+        
+        <p>Junte-se a nós nesta jornada de corações unidos, onde cada ação, por menor que seja, tem o potencial de fazer uma diferença monumental. Nossa missão é clara: <span>inspirar</span>, <span>capacitar</span> e <span>mobilizar</span>, tornando a compaixão uma força inegável para o bem em nosso mundo.</p>
+
+      </div>
+
+      <div class="col-md-5 direita" style="margin-top:10px; margin-bottom:10px">
+        <img src="./assets/images/recycling.jpeg" alt="" width="90%">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 esquerda">
 
         <h1 class="titulo">NOSSOS VALORES</h1>
 
@@ -92,17 +95,13 @@ if (!isset($_SESSION['id'])) {
           <li><strong>Inovação</strong>: Abraçamos a criatividade e a inovação como motores de mudança. Estamos sempre abertos a novas ideias e abordagens, buscando constantemente maneiras melhores de servir e impactar positivamente o mundo.</li>
         </ol>
 
-        Estes são os valores que nos definem como CaringCrafters. Eles nos inspiram a aspirar ao melhor e nos lembram do compromisso que temos com a construção de um mundo mais solidário, justo e inclusivo para todos.
-
+        Estes são os valores que nos definem como CaringCrafters. Eles nos inspiram a excelência e nos lembram do compromisso que temos com a construção de um mundo mais solidário, justo e inclusivo para todos.
       </div>
    
-
       <div class="col-md-5 direita" style="margin-top:10px; margin-bottom:10px">
         <img src="./assets/images/building2.jpeg" alt="" width="90%">
       </div>
-      </div>
     </div>
-  </div>
 
   </div>
 
